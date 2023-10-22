c67ef5be7d826726a0f63518c5befe2691fc8397
Krishna Priya B <123094497+krishna-priya-b@users.noreply.github.com>
2023-10-22 19:13:24 +0400

    22-10-2023

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+let score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+score = JSON.parse(localStorage.getItem('score'));
+
 let computerMove= '';
 
 function pickComputerMove(){
-    const randomNumber= Math.random()
+    const randomNumber= Math.random();
     
 
     if(randomNumber< 1/3){
@@ -17,22 +25,43 @@ function pickComputerMove(){
 }
 
 function game(myClick){
-    pickComputerMove()
+    pickComputerMove();
     let result = '';
 
     if(myClick === computerMove){
-        result = 'Tie';
+        result = 'Tie.';
     }
     else if(
         (myClick === 'Rock' && computerMove === 'Scissors') ||
         (myClick === 'Paper' && computerMove === 'Rock') ||
         (myClick === 'Scissors' && computerMove === 'Paper')
     ){
-        result = 'You Win';
+        result = 'You Win.';
     }
     else{
-        result='You lose'
+        result='You lose.';
     }
+
+
+
+    if(result==='You Win.'){
+        score.wins +=1;
+    }
+    else if (result==='You lose.'){
+        score.losses +=1;
+    }
+    else if( result==='Tie.'){
+        score.ties +=1;
+    }
+
+    //to store the score in the local storage.
+    localStorage.setItem('score', JSON.stringify(score));
  
-    alert('You picked '+ myClick + ' Computer picked '+computerMove+'. '+ result);
+    alert('You picked '+ myClick + '. Computer picked '+computerMove+'. '+ result + '\n'+ 'Wins: ' + score.wins + ', Losses: ' + score.losses + ', Ties: ' + score.ties );
+}
+
+function reset(){
+    score.wins=0;
+    score.losses=0;
+    score.ties=0;
 }
